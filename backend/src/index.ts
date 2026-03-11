bc38bb27a8bedf3ff2d2623578c7a7a37890c077
Makhadmahaska <mahadmahaska@hotmail.com>
2026-03-11 22:49:00 +0100

    add GET endpoint to fetch all games

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,20 @@
 import "dotenv/config";
 import express from "express";
 import prisma from "../lib/db.js";
-
+import gamesRouter from "./routes/games.js"
 const app = express();
 const PORT = 4000;
 
 app.use(express.json());
+
+
+app.use("/games", gamesRouter);
+
+
+
+
+
+
 
 
 
