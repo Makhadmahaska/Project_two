7a5cd3f116d79641912293cf8455abc99b2fd94a
Makhadmahaska <mahadmahaska@hotmail.com>
2026-03-11 17:32:13 +0100

    added index.ts with basic exress

---
+++ backend/index.ts
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+const PORT = 4000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API is running 🚀");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
