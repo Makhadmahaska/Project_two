45aa11b7cbf7081bdec35e3d0e5e6108a6e5fe6b
Makhadmahaska <mahadmahaska@hotmail.com>
2026-03-12 01:06:21 +0100

    impoted routes with logic

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,13 +2,18 @@ import "dotenv/config";
 import express from "express";
 import prisma from "../lib/db.js";
 import gamesRouter from "./routes/games.js"
+import usersRouter from "./routes/games.js"
+import sessionsRouter from "./routes/games.js"
+import statsRouter from "./routes/games.js"
 const app = express();
 const PORT = 4000;
 
 app.use(express.json());
 
-
-app.use("/games", gamesRouter);
+app.use('/users', usersRouter);
+app.use('/games', gamesRouter);
+app.use('/sessions', sessionsRouter);
+app.use('/stats', statsRouter);
 
 
 
